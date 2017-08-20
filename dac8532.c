7ad2b8f4947efe346f9bab5cc5a22f2adbbc04df
Stefan Wyss <wyss@superspider.net>
2017-08-20 21:45:50 +0200

    replaced names

--- dac8532.c
+++ dac8532.c
@@ -1,7 +1,7 @@
 /*
- * AD5686R, AD5685R, AD5684R Digital to analog converters  driver
+ * DAC8532/DAC8552 Digital to analog converters  driver
  *
- * Copyright 2011 Analog Devices Inc.
+ * Copyright 2017 wyss@superspider.net
  *
  * Licensed under the GPL-2.
  */
@@ -15,43 +15,38 @@
 #include <linux/slab.h>
 #include <linux/sysfs.h>
 #include <linux/regulator/consumer.h>
-
 #include <linux/iio/iio.h>
 #include <linux/iio/sysfs.h>
 
-#define AD5686_DAC_CHANNELS			4
-
-#define AD5686_ADDR(x)				((x) << 16)
-#define AD5686_CMD(x)				((x) << 20)
-
-#define AD5686_ADDR_DAC(chan)		(0x1 << (chan))
-#define AD5686_ADDR_ALL_DAC			0xF
-
-#define AD5686_CMD_NOOP				0x0
-#define AD5686_CMD_WRITE_INPUT_N		0x1
-#define AD5686_CMD_UPDATE_DAC_N			0x2
-#define AD5686_CMD_WRITE_INPUT_N_UPDATE_N	0x3
-#define AD5686_CMD_POWERDOWN_DAC		0x4
-#define AD5686_CMD_LDAC_MASK			0x5
-#define AD5686_CMD_RESET			0x6
-#define AD5686_CMD_INTERNAL_REFER_SETUP		0x7
-#define AD5686_CMD_DAISY_CHAIN_ENABLE		0x8
-#define AD5686_CMD_READBACK_ENABLE		0x9
-
-#define AD5686_LDAC_PWRDN_NONE			0x0
-#define AD5686_LDAC_PWRDN_1K			0x1
-#define AD5686_LDAC_PWRDN_100K			0x2
-#define AD5686_LDAC_PWRDN_3STATE		0x3
+#define DAC8532_DAC_CHANNELS			4
+#define DAC8532_ADDR(x)				((x) << 16)
+#define DAC8532_CMD(x)				((x) << 20)
+#define DAC8532_ADDR_DAC(chan)		(0x1 << (chan))
+#define DAC8532_ADDR_ALL_DAC			0xF
+#define DAC8532_CMD_NOOP				0x0
+#define DAC8532_CMD_WRITE_INPUT_N		0x1
+#define DAC8532_CMD_UPDATE_DAC_N			0x2
+#define DAC8532_CMD_WRITE_INPUT_N_UPDATE_N	0x3
+#define DAC8532_CMD_POWERDOWN_DAC		0x4
+#define DAC8532_CMD_LDAC_MASK			0x5
+#define DAC8532_CMD_RESET			0x6
+#define DAC8532_CMD_INTERNAL_REFER_SETUP		0x7
+#define DAC8532_CMD_DAISY_CHAIN_ENABLE		0x8
+#define DAC8532_CMD_READBACK_ENABLE		0x9
+#define DAC8532_LDAC_PWRDN_NONE			0x0
+#define DAC8532_LDAC_PWRDN_1K			0x1
+#define DAC8532_LDAC_PWRDN_100K			0x2
+#define DAC8532_LDAC_PWRDN_3STATE		0x3
 
 /**
- * struct ad5686_chip_info - chip specific information
- * @int_vref_mv:	AD5620/40/60: the internal reference voltage
+ * struct dac8532_chip_info - chip specific information
+ * @int_vref_mv:	DAC8532: the internal reference voltage
  * @channel:		channel specification
 */
 
-struct ad5686_chip_info {
+struct dac8532_chip_info {
 	u16				int_vref_mv;
-	struct iio_chan_spec		channel[AD5686_DAC_CHANNELS];
+	struct iio_chan_spec		channel[DAC8532_DAC_CHANNELS];
 };
 
 /**
@@ -65,9 +60,9 @@ struct ad5686_chip_info {
  * @data:		spi transfer buffers
  */
 
-struct ad5686_state {
+struct dac8532_state {
 	struct spi_device		*spi;
-	const struct ad5686_chip_info	*chip_info;
+	const struct dac8532_chip_info	*chip_info;
 	struct regulator		*reg;
 	unsigned short			vref_mv;
 	unsigned			pwr_down_mask;
@@ -84,27 +79,26 @@ struct ad5686_state {
 };
 
 /**
- * ad5686_supported_device_ids:
+ * dac8532_supported_device_ids:
  */
 
-enum ad5686_supported_device_ids {
-	ID_AD5684,
-	ID_AD5685,
-	ID_AD5686,
+enum dac8532_supported_device_ids {
+	ID_DAC8532,
+	ID_DAC8552,
 };
-static int ad5686_spi_write(struct ad5686_state *st,
+static int dac8532_spi_write(struct dac8532_state *st,
 			     u8 cmd, u8 addr, u16 val, u8 shift)
 {
 	val <<= shift;
 
-	st->data[0].d32 = cpu_to_be32(AD5686_CMD(cmd) |
-			      AD5686_ADDR(addr) |
+	st->data[0].d32 = cpu_to_be32(DAC8532_CMD(cmd) |
+			      DAC8532_ADDR(addr) |
 			      val);
 
 	return spi_write(st->spi, &st->data[0].d8[1], 3);
 }
 
-static int ad5686_spi_read(struct ad5686_state *st, u8 addr)
+static int dac8532_spi_read(struct dac8532_state *st, u8 addr)
 {
 	struct spi_transfer t[] = {
 		{
@@ -119,9 +113,9 @@ static int ad5686_spi_read(struct ad5686_state *st, u8 addr)
 	};
 	int ret;
 
-	st->data[0].d32 = cpu_to_be32(AD5686_CMD(AD5686_CMD_READBACK_ENABLE) |
-			      AD5686_ADDR(addr));
-	st->data[1].d32 = cpu_to_be32(AD5686_CMD(AD5686_CMD_NOOP));
+	st->data[0].d32 = cpu_to_be32(DAC8532_CMD(DAC8532_CMD_READBACK_ENABLE) |
+			      DAC8532_ADDR(addr));
+	st->data[1].d32 = cpu_to_be32(DAC8532_CMD(DAC8532_CMD_NOOP));
 
 	ret = spi_sync_transfer(st->spi, t, ARRAY_SIZE(t));
 	if (ret < 0)
@@ -130,24 +124,24 @@ static int ad5686_spi_read(struct ad5686_state *st, u8 addr)
 	return be32_to_cpu(st->data[2].d32);
 }
 
-static const char * const ad5686_powerdown_modes[] = {
+static const char * const dac8532_powerdown_modes[] = {
 	"1kohm_to_gnd",
 	"100kohm_to_gnd",
 	"three_state"
 };
 
-static int ad5686_get_powerdown_mode(struct iio_dev *indio_dev,
+static int dac8532_get_powerdown_mode(struct iio_dev *indio_dev,
 	const struct iio_chan_spec *chan)
 {
-	struct ad5686_state *st = iio_priv(indio_dev);
+	struct dac8532_state *st = iio_priv(indio_dev);
 
 	return ((st->pwr_down_mode >> (chan->channel * 2)) & 0x3) - 1;
 }
 
-static int ad5686_set_powerdown_mode(struct iio_dev *indio_dev,
+static int dac8532_set_powerdown_mode(struct iio_dev *indio_dev,
 	const struct iio_chan_spec *chan, unsigned int mode)
 {
-	struct ad5686_state *st = iio_priv(indio_dev);
+	struct dac8532_state *st = iio_priv(indio_dev);
 
 	st->pwr_down_mode &= ~(0x3 << (chan->channel * 2));
 	st->pwr_down_mode |= ((mode + 1) << (chan->channel * 2));
@@ -155,29 +149,29 @@ static int ad5686_set_powerdown_mode(struct iio_dev *indio_dev,
 	return 0;
 }
 
-static const struct iio_enum ad5686_powerdown_mode_enum = {
-	.items = ad5686_powerdown_modes,
-	.num_items = ARRAY_SIZE(ad5686_powerdown_modes),
-	.get = ad5686_get_powerdown_mode,
-	.set = ad5686_set_powerdown_mode,
+static const struct iio_enum dac8532_powerdown_mode_enum = {
+	.items = dac8532_powerdown_modes,
+	.num_items = ARRAY_SIZE(dac8532_powerdown_modes),
+	.get = dac8532_get_powerdown_mode,
+	.set = dac8532_set_powerdown_mode,
 };
 
-static ssize_t ad5686_read_dac_powerdown(struct iio_dev *indio_dev,
+static ssize_t dac8532_read_dac_powerdown(struct iio_dev *indio_dev,
 	uintptr_t private, const struct iio_chan_spec *chan, char *buf)
 {
-	struct ad5686_state *st = iio_priv(indio_dev);
+	struct dac8532_state *st = iio_priv(indio_dev);
 
 	return sprintf(buf, "%d\n", !!(st->pwr_down_mask &
 			(0x3 << (chan->channel * 2))));
 }
 
-static ssize_t ad5686_write_dac_powerdown(struct iio_dev *indio_dev,
+static ssize_t dac8532_write_dac_powerdown(struct iio_dev *indio_dev,
 	 uintptr_t private, const struct iio_chan_spec *chan, const char *buf,
 	 size_t len)
 {
 	bool readin;
 	int ret;
-	struct ad5686_state *st = iio_priv(indio_dev);
+	struct dac8532_state *st = iio_priv(indio_dev);
 
 	ret = strtobool(buf, &readin);
 	if (ret)
@@ -188,25 +182,25 @@ static ssize_t ad5686_write_dac_powerdown(struct iio_dev *indio_dev,
 	else
 		st->pwr_down_mask &= ~(0x3 << (chan->channel * 2));
 
-	ret = ad5686_spi_write(st, AD5686_CMD_POWERDOWN_DAC, 0,
+	ret = dac8532_spi_write(st, DAC8532_CMD_POWERDOWN_DAC, 0,
 			       st->pwr_down_mask & st->pwr_down_mode, 0);
 
 	return ret ? ret : len;
 }
 
-static int ad5686_read_raw(struct iio_dev *indio_dev,
+static int dac8532_read_raw(struct iio_dev *indio_dev,
 			   struct iio_chan_spec const *chan,
 			   int *val,
 			   int *val2,
 			   long m)
 {
-	struct ad5686_state *st = iio_priv(indio_dev);
+	struct dac8532_state *st = iio_priv(indio_dev);
 	int ret;
 
 	switch (m) {
 	case IIO_CHAN_INFO_RAW:
 		mutex_lock(&indio_dev->mlock);
-		ret = ad5686_spi_read(st, chan->address);
+		ret = dac8532_spi_read(st, chan->address);
 		mutex_unlock(&indio_dev->mlock);
 		if (ret < 0)
 			return ret;
@@ -220,13 +214,13 @@ static int ad5686_read_raw(struct iio_dev *indio_dev,
 	return -EINVAL;
 }
 
-static int ad5686_write_raw(struct iio_dev *indio_dev,
+static int dac8532_write_raw(struct iio_dev *indio_dev,
 			       struct iio_chan_spec const *chan,
 			       int val,
 			       int val2,
 			       long mask)
 {
-	struct ad5686_state *st = iio_priv(indio_dev);
+	struct dac8532_state *st = iio_priv(indio_dev);
 	int ret;
 
 	switch (mask) {
@@ -235,8 +229,8 @@ static int ad5686_write_raw(struct iio_dev *indio_dev,
 			return -EINVAL;
 
 		mutex_lock(&indio_dev->mlock);
-		ret = ad5686_spi_write(st,
-				 AD5686_CMD_WRITE_INPUT_N_UPDATE_N,
+		ret = dac8532_spi_write(st,
+				 DAC8532_CMD_WRITE_INPUT_N_UPDATE_N,
 				 chan->address,
 				 val,
 				 chan->scan_type.shift);
@@ -249,69 +243,62 @@ static int ad5686_write_raw(struct iio_dev *indio_dev,
 	return ret;
 }
 
-static const struct iio_info ad5686_info = {
-	.read_raw = ad5686_read_raw,
-	.write_raw = ad5686_write_raw,
+static const struct iio_info dac8532_info = {
+	.read_raw = dac8532_read_raw,
+	.write_raw = dac8532_write_raw,
 	.driver_module = THIS_MODULE,
 };
 
-static const struct iio_chan_spec_ext_info ad5686_ext_info[] = {
+static const struct iio_chan_spec_ext_info dac8532_ext_info[] = {
 	{
 		.name = "powerdown",
-		.read = ad5686_read_dac_powerdown,
-		.write = ad5686_write_dac_powerdown,
+		.read = dac8532_read_dac_powerdown,
+		.write = dac8532_write_dac_powerdown,
 		.shared = IIO_SEPARATE,
 	},
-	IIO_ENUM("powerdown_mode", IIO_SEPARATE, &ad5686_powerdown_mode_enum),
-	IIO_ENUM_AVAILABLE("powerdown_mode", &ad5686_powerdown_mode_enum),
+	IIO_ENUM("powerdown_mode", IIO_SEPARATE, &dac8532_powerdown_mode_enum),
+	IIO_ENUM_AVAILABLE("powerdown_mode", &dac8532_powerdown_mode_enum),
 	{ },
 };
 
-#define AD5868_CHANNEL(chan, bits, _shift) {			\
+#define DAC8532_CHANNEL(chan, bits, _shift) {			\
 		.type = IIO_VOLTAGE,				\
 		.indexed = 1,					\
 		.output = 1,					\
 		.channel = chan,				\
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),	\
 		.info_mask_shared_by_type = BIT(IIO_CHAN_INFO_SCALE),\
-		.address = AD5686_ADDR_DAC(chan),		\
+		.address = DAC8532_ADDR_DAC(chan),		\
 		.scan_type = {					\
 			.sign = 'u',				\
 			.realbits = (bits),			\
 			.storagebits = 16,			\
 			.shift = (_shift),			\
 		},						\
-		.ext_info = ad5686_ext_info,			\
+		.ext_info = dac8532_ext_info,			\
 }
 
-static const struct ad5686_chip_info ad5686_chip_info_tbl[] = {
-	[ID_AD5684] = {
-		.channel[0] = AD5868_CHANNEL(0, 12, 4),
-		.channel[1] = AD5868_CHANNEL(1, 12, 4),
-		.channel[2] = AD5868_CHANNEL(2, 12, 4),
-		.channel[3] = AD5868_CHANNEL(3, 12, 4),
+static const struct dac8532_chip_info dac8532_chip_info_tbl[] = {
+	[ID_DAC8552] = {
+		.channel[0] = DAC8532_CHANNEL(0, 14, 2),
+		.channel[1] = DAC8532_CHANNEL(1, 14, 2),
+		.channel[2] = DAC8532_CHANNEL(2, 14, 2),
+		.channel[3] = DAC8532_CHANNEL(3, 14, 2),
 		.int_vref_mv = 2500,
 	},
-	[ID_AD5685] = {
-		.channel[0] = AD5868_CHANNEL(0, 14, 2),
-		.channel[1] = AD5868_CHANNEL(1, 14, 2),
-		.channel[2] = AD5868_CHANNEL(2, 14, 2),
-		.channel[3] = AD5868_CHANNEL(3, 14, 2),
-		.int_vref_mv = 2500,
-	},
-	[ID_AD5686] = {
-		.channel[0] = AD5868_CHANNEL(0, 16, 0),
-		.channel[1] = AD5868_CHANNEL(1, 16, 0),
-		.channel[2] = AD5868_CHANNEL(2, 16, 0),
-		.channel[3] = AD5868_CHANNEL(3, 16, 0),
+	[ID_DAC8532] = {
+		.channel[0] = DAC8532_CHANNEL(0, 16, 0),
+		.channel[1] = DAC8532_CHANNEL(1, 16, 0),
+		.channel[2] = DAC8532_CHANNEL(2, 16, 0),
+		.channel[3] = DAC8532_CHANNEL(3, 16, 0),
 		.int_vref_mv = 2500,
 	},
 };
 
 
-static int ad5686_probe(struct spi_device *spi)
+static int dac8532_probe(struct spi_device *spi)
 {
-	struct ad5686_state *st;
+	struct dac8532_state *st;
 	struct iio_dev *indio_dev;
 	int ret, voltage_uv = 0;
 
@@ -336,7 +323,7 @@ static int ad5686_probe(struct spi_device *spi)
 	}
 
 	st->chip_info =
-		&ad5686_chip_info_tbl[spi_get_device_id(spi)->driver_data];
+		&dac8532_chip_info_tbl[spi_get_device_id(spi)->driver_data];
 
 	if (voltage_uv)
 		st->vref_mv = voltage_uv / 1000;
@@ -350,12 +337,12 @@ static int ad5686_probe(struct spi_device *spi)
 
 	indio_dev->dev.parent = &spi->dev;
 	indio_dev->name = spi_get_device_id(spi)->name;
-	indio_dev->info = &ad5686_info;
+	indio_dev->info = &dac8532_info;
 	indio_dev->modes = INDIO_DIRECT_MODE;
 	indio_dev->channels = st->chip_info->channel;
-	indio_dev->num_channels = AD5686_DAC_CHANNELS;
+	indio_dev->num_channels = DAC8532_DAC_CHANNELS;
 
-	ret = ad5686_spi_write(st, AD5686_CMD_INTERNAL_REFER_SETUP, 0,
+	ret = dac8532_spi_write(st, DAC8532_CMD_INTERNAL_REFER_SETUP, 0,
 				!!voltage_uv, 0);
 	if (ret)
 		goto error_disable_reg;
@@ -372,10 +359,10 @@ error_disable_reg:
 	return ret;
 }
 
-static int ad5686_remove(struct spi_device *spi)
+static int dac8532_remove(struct spi_device *spi)
 {
 	struct iio_dev *indio_dev = spi_get_drvdata(spi);
-	struct ad5686_state *st = iio_priv(indio_dev);
+	struct dac8532_state *st = iio_priv(indio_dev);
 
 	iio_device_unregister(indio_dev);
 	if (!IS_ERR(st->reg))
@@ -384,24 +371,23 @@ static int ad5686_remove(struct spi_device *spi)
 	return 0;
 }
 
-static const struct spi_device_id ad5686_id[] = {
-	{"ad5684", ID_AD5684},
-	{"ad5685", ID_AD5685},
-	{"ad5686", ID_AD5686},
+static const struct spi_device_id dac8532_id[] = {
+	{"dac8552", ID_DAC8552},
+	{"dac8532", ID_DAC8532},
 	{}
 };
-MODULE_DEVICE_TABLE(spi, ad5686_id);
+MODULE_DEVICE_TABLE(spi, dac8532_id);
 
-static struct spi_driver ad5686_driver = {
+static struct spi_driver dac8532_driver = {
 	.driver = {
-		   .name = "ad5686",
+		   .name = "dac8532",
 		   },
-	.probe = ad5686_probe,
-	.remove = ad5686_remove,
-	.id_table = ad5686_id,
+	.probe = dac8532_probe,
+	.remove = dac8532_remove,
+	.id_table = dac8532_id,
 };
-module_spi_driver(ad5686_driver);
+module_spi_driver(dac8532_driver);
 
 MODULE_AUTHOR("Michael Hennerich <hennerich@blackfin.uclinux.org>");
-MODULE_DESCRIPTION("Analog Devices AD5686/85/84 DAC");
+MODULE_DESCRIPTION("Analog Devices DAC8532/85/84 DAC");
 MODULE_LICENSE("GPL v2");
